35c239916442985d5d275aff959ffd899ae74a9f
Krzysztof Piaskowy <krzychu2956@gmail.com>
2018-03-01 18:58:39 +0100

    class Car

---
+++ src/hashCode/Car.java
@@ -0,0 +1,17 @@
+package hashCode;
+
+public class Car {
+	public int x = 0;
+	public int y = 0;
+	public boolean free;
+	
+	public Car(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setPosition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
--- src/hashCode/Krzysiek.java
+++
@@ -1,5 +0,0 @@
-package hashCode;
-
-public class Krzysiek {
-	
-}
