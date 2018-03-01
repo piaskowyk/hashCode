11d65fe117f5eaf931fbafba83d63a7ffd6080ac
Krzysztof Piaskowy <krzychu2956@gmail.com>
2018-03-01 19:32:14 +0100

    tmp

--- src/hashCode/Car.java
+++ src/hashCode/Car.java
@@ -1,11 +1,13 @@
 package hashCode;
 
+import java.awt.Point;
+
 public class Car {
 	public int x = 0;
 	public int y = 0;
 	public int freeTour = 0;
 	public boolean free;
-
+	
 	public Car(int x, int y) {
 		this.x = x;
 		this.y = y;
@@ -21,8 +23,15 @@ public class Car {
 		int minDist = min.getDistance();
 		int tmp = 0;
 		int how = rides.length;
-		Point point;
+		Point pointR = null;
+		Point pointC = null;
 		for(int i=0; i<how; i++) {
+			pointC.x = this.x;
+			pointC.y = this.y;
+			
+			pointR.x = rides[i].startPoint.x;
+			pointR.y = rides[i].startPoint.y;
+			
 			tmp = rides[i].getDistance();
 			if(tmp < minDist) {
 				minDist = tmp;
